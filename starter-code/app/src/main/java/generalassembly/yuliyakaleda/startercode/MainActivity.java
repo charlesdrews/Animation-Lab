e3845fd55513b6e4fedd209e96392507973a9d67
charlesdrews <charlesdrews@gmail.com>
2016-02-21 22:10:36 -0500

    Add remove wish functionality; fix add wish to add to top of list

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -1,15 +1,19 @@
 package generalassembly.yuliyakaleda.startercode;
 
 import android.animation.LayoutTransition;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
   EditText mInput;
@@ -35,6 +39,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.button:
+        addWish();
+        break;
+      default:
+        if ( ((ViewGroup) v.getParent()).getId() == R.id.wish_list ) {
+          removeWish( (TextView) v );
+        }
+        break;
+    }
+  }
+
+  private void addWish() {
     String wish = mInput.getText().toString();
     if (wish.isEmpty()) {
       mInput.setError("Enter your wish first!");
@@ -49,7 +66,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView listItem = new TextView(MainActivity.this);
     listItem.setText(wish);
+    listItem.setPadding(8, 8, 8, 8);
+    listItem.setOnClickListener(MainActivity.this);
 
-    mWishList.addView(listItem);
+    mWishList.addView(listItem, 0);
+  }
+
+  private void removeWish(final TextView v) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+    builder.setTitle("Remove Wish");
+    builder.setMessage("Remove " + v.getText() + " from your wish list?");
+
+    builder.setNegativeButton("Cancel", null);
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        mWishList.removeView(v);
+        dialog.dismiss();
+        Toast.makeText(MainActivity.this, "remove", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    builder.show();
   }
 }
